22777f7fa86a5daf4484eafacb08c4e2c515ef21
Karl Beucke <karl.beucke@uni-weimar.de>
2023-07-04 21:19:31 +0200

    Main Lager Last

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -337,6 +337,7 @@ public partial class MainWindow
         }
 
         _darstellung = new Darstellung(_dlt!, VisualErgebnisse);
+        bbbbbb
         _darstellung.FestlegungAuflösung();
         _darstellung.TrägerDarstellen();
         _darstellung.Momentenverlauf();
